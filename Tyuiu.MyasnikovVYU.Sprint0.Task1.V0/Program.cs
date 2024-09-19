889cd174eb6e7f8c1ee6ac84242610c1de977312
Tyuiu.MyasnikovVYU@outlook.com <wova1432@gmail.com>
2024-09-20 03:41:15 +0500

    Task2

--- Tyuiu.MyasnikovVYU.Sprint0.Task1.V0/Program.cs
+++ Tyuiu.MyasnikovVYU.Sprint0.Task1.V0/Program.cs
@@ -1,17 +1,9 @@
-﻿namespace Tyuiu.MyasnikovVYU.Sprint0.Task1.V0
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
 
-            string name = "Владимир";
-            int age = 19;
+string name = "Владимир";
+int age = 19;
 
-            Console.WriteLine(Здравствуйте меня зовут " + name + " мне " + age + " лет.");
+Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет.");
 
-            Console.ReadKey();
-        }
-    }
-}
+
+Console.ReadKey();
---
+++ Tyuiu.MyasnikovVYU.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.MyasnikovVYU.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.MyasnikovVYU.Sprint0.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Владимир"));
+            Console.ReadKey();
+        }
+    }
+}
